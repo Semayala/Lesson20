55571471f985f1d6b1a721d26dc17d20a4d9588c
Andrea <semayala20@gmail.com>
2024-11-30 11:03:10 +0000

    ranamed .py file and wrote a program, which created an output.txt file

--- count_words.py
+++ count_words.py
@@ -1,17 +1,19 @@
+import re
 from collections import Counter
+
 
 word_count = Counter()
 
+
 with open('input.txt', encoding='utf-8') as input_file:
     for line in input_file:
-        words = line.split(' ')
+        words = re.findall(r'\b\w{4,}\b', line.lower())
         word_count.update(words)
         print(word_count)
 
 most_common_words = word_count.most_common(3)
+print(most_common_words)
 
-for word in most_common_words:
-    with open('output.txt', 'w', encoding='utf-8') as output_file:
-        output_file.write(f'{most_common_words}')
-
-
+with open('output.txt', 'w', encoding='utf-8') as output_file:
+    for word, count in most_common_words:
+        output_file.write(f'{word}: {count}\n')
